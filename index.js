d8cd363c89acfe49b193ea38811aeabb2d86d6da
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-04 23:35:57 +0600

    nodejs

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('./app')
+
+// console.log(app)
+// console.log(app.z());
+// console.log("welcome to my node js");
+
+const arr = [1,2,5,4,3,1,5,7,1];
+
+// let result = arr.filter((item)=>{
+//     return item>3;
+//     // console.log(item);
+// });
+
+// console.log(result);
