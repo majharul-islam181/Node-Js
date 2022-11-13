5a7200ed57fd8f16fe3f9a5243b33d4119bf0eb6
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-13 20:48:55 +0600

    package.json added and deleted node module

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const app = require('./app')
 const fs = require('fs'); //non global module 
+
 // const chamokisfilesytem = require('fs'); // variable(chamokisfilesystem) can be anything 
 
 // console.log(app)
 // console.log(app.z());
 // console.log("welcome to my node js");
 
-const arr = [1,2,5,4,3,1,5,7,1];  
-console.log('this is output');
+// const arr = [1,2,5,4,3,1,5,7,1];  
+// console.log('this is output');
 
 // let result = arr.filter((item)=>{
 //     return item>3;
@@ -15,12 +16,12 @@ console.log('this is output');
 // });
 
 // console.log(result);
-console.log('code step by step');
-fs.writeFileSync('hello.txt','chamok'); //it'll create hello.txt file
+// console.log('code step by step');
+// fs.writeFileSync('hello.txt','chamok'); //it'll create hello.txt file
 // chamokisfilesytem.writeFileSync('temp.txt',"this is temp file");
 
-console.log("dic name: ",__dirname);
-console.log('file name: ',__filename);
+// console.log("dicetoray name: ",__dirname);
+// console.log('file name: ',__filename);
 
 // const fs = require('fs').writeFileSync('another.txt','another file'); // if we dont want to add all fs modules function.
 
@@ -52,6 +53,24 @@ http.createServer(dataControl).listen(4000);
 
 // Package.json
 
+
+// colors package
+const colorpackage = require('colors');
+console.log('this is a black'.black);
+console.log('this is a blue'.blue);
+console.log('this is a red'.red);
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap);
+
+
+
+//chalk npm
+// import chalk from 'chalk';
+
+// console.log(chalk.blue('Hello world!'));
 
 
 
