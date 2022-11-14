29aac2865823ad53b588f975df464b238524edfd
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-14 12:55:59 +0600

    update

--- index.js
+++ index.js
@@ -82,32 +82,6 @@ http.createServer((req, res) => {
   }).listen(600);
 */
 
-  
-//########  Show File List From File System
-
-
-const fs = require('fs');
-const path = require('path');
-const dirPath = path.join(__dirname,'files');
-
-//Write File 
-/*
-for(i=0;i<5;i++){
-    fs.writeFileSync(dirPath+'/hello'+i+'.txt','a sample text file');
-}
-*/
-
-// Read File
-//directory read => then file read
-/*
-fs.readdir(dirPath,(err,files)=>{
-    // console.log(files); //it gives us in a Array format
-    files.forEach((items)=>{
-        console.log(items);
-    });
-});
-*/
-
 
 
  
