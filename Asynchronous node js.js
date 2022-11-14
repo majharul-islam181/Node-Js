65a027b5c99323316c7c6affb3b1e3e062208c5b
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-14 23:26:51 +0600

    Asynchronous node js

---
+++ Asynchronous node js.js
@@ -0,0 +1,41 @@
+// console.log('Start exe...1');
+
+// console.log('Start exe.....2');
+
+// console.log('Start exe........3');
+/*
+Start exe...1
+Start exe.....2
+Start exe........3 
+*/
+
+
+console.log('Start exe...1');
+
+setTimeout(() => {
+    console.log('Start exe.....2');
+}, 2000);
+console.log('Start exe........3');
+
+
+/*
+Start exe...1
+Start exe........3
+Start exe.....2(after 2000 it will exicute)
+*/
+
+/*drawback 
+
+let a=10;
+let b=0;
+
+setTimeout(() => {
+    b=20;
+}, 2000);
+console.log(a+b);
+
+*/
+
+/*
+10
+*/
