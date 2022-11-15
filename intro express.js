fdb0c406070b4e2535c2cdaf1e0a66c2a25d6b27
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-15 12:56:37 +0600

    intro to express js

---
+++ intro express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('',(req,res)=>{
+    console.log('Data sent by ',req.query.name);
+    
+    res.send('Hello, this is home page create by :'+req.query.name);
+});
+
+
+app.get('/about',(req,res)=>{
+    res.send('Hello, this is about page');
+});
+
+
+app.get('/profile',(req,res)=>{
+    res.send('Hello, this is profile sections');
+});
+
+app.get('/name',(req,res)=>{
+    res.send('Hello, this is name section');
+});
+app.listen(5400); 
