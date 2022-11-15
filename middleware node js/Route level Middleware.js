a9756bd83b19e8f4cdb8d318c9a9fcfed143e77a
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-15 23:37:26 +0600

    route level middleware

---
+++ middleware node js/Route level Middleware.js
@@ -0,0 +1,27 @@
+// Single or Route level middleware 
+
+
+const express = require("express");
+const app = express();
+
+const reqFilter = (req, res, next) => {
+  if (!req.query.age) {
+    res.send("Please provide age");
+
+  }else if(req.query.age<18){
+    res.send('You cannot access this page, You entered :'+req.query.age);
+  }
+   else {
+    next();
+  }
+};
+
+
+app.get("/",reqFilter, (req, res) => { //only this routes have restrictions
+  res.send("welcome to home page, Your age is: "+req.query.age);
+});
+
+app.get("/user", (req, res) => {
+  res.send("welcome to user page, Your age is: "+req.query.age);
+});
+app.listen(5555);
