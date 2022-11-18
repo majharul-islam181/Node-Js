b893372416c772c5d8187acf66e9c5ca59b3a8b0
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-19 02:18:15 +0600

    mongoose

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/teapack");
-
 const main = async () => {
+ await mongoose.connect("mongodb://0.0.0.0:27017/teapack");
   const ProductSchema = new mongoose.Schema({
     name: String,
+    price: Number
   });
 
-  const ProductModel = mongoose.model("bzar", ProductSchema);
-  let data = new ProductModel({ name: "m8" });
+  const ProductModel = mongoose.model("bazar", ProductSchema);
+  let data = new ProductModel({ name: "Iphone 13",price:50000});
   let result = await data.save();
   console.log(result);
 };
