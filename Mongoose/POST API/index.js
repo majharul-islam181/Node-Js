e5312c6b78117b303e6c20b026815b98062455e4
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-20 00:30:02 +0600

    GET, DELETE AND PUT APIs with node js and mongoose

--- Mongoose/POST API/index.js
+++ Mongoose/POST API/index.js
@@ -1,30 +1,28 @@
-const express = require('express');
-require('./config');
-const Product = require('./product');
+const express = require("express");
+require("./config");
+const Product = require("./product");
 const app = express();
 app.use(express.json());
 
-app.post('/create',async (req,res)=>{
-
-    let data = new Product(req.body);
-    let result = await data.save();
-    console.log(result);
-    res.send('Succesfully done');
-    // console.log(req.body);
-    // res.send('done');
-
+app.post("/create", async (req, res) => {
+  let data = new Product(req.body);
+  let result = await data.save();
+  console.log(result);
+  res.send("Succesfully done");
+  // console.log(req.body);
+  // res.send('done');
 });
 
-app.get('/list',async(req,res)=>{
-    let data =await Product.find({price: '400'});
-    res.send(data);
-})
+app.get("/list", async (req, res) => {
+  let data = await Product.find({ price: "400" });
+  res.send(data);
+});
 
 //delete with id  // 637915f5b18d42091f9ab1dc
-app.delete('/delete/:_id',async (req,res)=>{
-    console.log(req.params);
-    let data =await Product.deleteOne(req.params);
-    res.send(data);
+app.delete("/delete/:_id", async (req, res) => {
+  console.log(req.params);
+  let data = await Product.deleteOne(req.params);
+  res.send(data);
 });
 
 //delete with name
@@ -33,25 +31,21 @@ app.delete('/delete/:_id',async (req,res)=>{
 //     res.send(data);
 // });
 
-app.delete('/delete/:_id',async (req,res)=>{
-    console.log(req.params);
-    let data =await Product.deleteOne(req.params);
-    res.send(data);
+app.delete("/delete/:_id", async (req, res) => {
+  console.log(req.params);
+  let data = await Product.deleteOne(req.params);
+  res.send(data);
 });
 
-app.put('/update',async(req,res)=>{
+app.put("/update", async (req, res) => {
+  let data = await Product.updateMany(
+    { name: "Iphone" },
+    {
+      $set: { price: 20 },
+    }
+  );
 
-    let data = await Product.updateMany({name: 'Iphone'},
-        {
-            $set: {price: 20}
-        }
-        );
-
-    res.send('update successfull')
-
+  res.send("update successfull");
 });
 
 app.listen(5000);
-
-
-
