76861082be2709805b222271221c283c7a89bf3a
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-19 23:41:59 +0600

    post api

---
+++ Mongoose/POST API/index.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+require('./config');
+const Product = require('./product');
+
+const app = express();
+app.use(express.json());
+
+app.post('/create',async (req,res)=>{
+
+    let data = new Product(req.body);
+    let result = await data.save();
+    
+
+
+    // console.log(req.body);
+    // res.send('done');
+
+})
+
+app.listen(5000);
