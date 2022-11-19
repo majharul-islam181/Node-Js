d275c1e308c161738c3bb99dc97dc6a839df1fe1
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-11-20 00:09:36 +0600

    get delete api

--- Mongoose/POST API/index.js
+++ Mongoose/POST API/index.js
@@ -10,12 +10,31 @@ app.post('/create',async (req,res)=>{
 
     let data = new Product(req.body);
     let result = await data.save();
-    
-
-
+    console.log(result);
+    res.send('Succesfully done');
     // console.log(req.body);
     // res.send('done');
 
+});
+
+app.get('/list',async(req,res)=>{
+    let data =await Product.find({price: '400'});
+    res.send(data);
 })
 
+//delete with id
+app.delete('/delete/:_id',async (req,res)=>{
+    console.log(req.params);
+    let data =await Product.deleteOne(req.params);
+    res.send(data);
+});
+
+// app.delete('/delete',async (req,res)=>{
+//     let data =await Product.deleteOne({name: 'm 40'});
+//     res.send(data);
+// });
+
 app.listen(5000);
+
+// 637915f5b18d42091f9ab1dc
+
